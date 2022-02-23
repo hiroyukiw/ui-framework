96484e45cf8ffb5029cad175437f3c847d566436
Hiroyuki.Wakamiya <Hiroyuki.Wakamiya@2nd-community.com>
2022-02-23 21:51:33 +0900

    sass add

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p class="content">ヘッダー</p>
+    <nuxt />
+    <p>フッター</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'DefaultLayout',
+})
+</script>
